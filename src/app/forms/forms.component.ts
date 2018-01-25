00451fb5c48997ce76a97095996cc93384aaa741
samuelDorado <samuel.dorado@sngular.team>
2018-01-25 20:18:17 +0100

    animations

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -3,12 +3,26 @@ import { FieldsService } from '../services/fields.service';
 import { FieldControlService } from '../services/field-control.service';
 import { FieldBase } from '../form-field-models/field-base';
 import { FormGroup } from '@angular/forms';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
   styleUrls: ['./forms.component.css'],
-  providers:[FieldsService,FieldControlService]
+  providers:[FieldsService,FieldControlService],
+  animations:[trigger('flyInOut', [
+      state('in', style({transform: 'translateX(0)'})),
+      transition('void => *', [
+        style({transform: 'translateX(-100%)'}),
+        animate(300)
+      ])
+    ])]
 })
 export class FormsComponent implements OnInit {
   fields: FieldBase<any>[];
